e64dd4a80cae663559c89208826467417911a5ef
esraa <israamohamed212100@gmail.com>
2023-12-18 19:47:41 +0200

    new

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -8,24 +8,17 @@ import port5 from"../../Images/port5.jpg"
 export default function Portfolio() {
   return (
     <section id="Portfolio">
-    <h5>My Recent Work</h5>
+   
     <h2>Portfolio</h2>
     <div className="container portfolio_container">
     <article className='portfolio_item' data-aos="fade-left">
     <div className="portfolio_item-image">
     <img src={port1}/>
     </div>
-<<<<<<< HEAD
     <h3>Ecommerce Website</h3>
     <div className="portfolio_item-cta">
     <a href="https://github.com" className='btn' target="_blank">Gighub</a>
     <a href="https://esraamohamed2023.github.io/Task-10024/" className='btn btn-primary' target="_blank">Live Demo</a>
-=======
-    <h3>Bootstrap_project</h3>
-    <div className="portfolio_item-cta">
-    <a href="https://github.com" className='btn' target="_blank">Gighub</a>
-    <a href="https://esraamohamed2023.github.io/Bootstrap_project/" className='btn btn-primary' target="_blank">Live Demo</a>
->>>>>>> 15105bae0add1c9c82a2f5129dc5ce2f2764bbfb
     </div>
     </article>
     <article className='portfolio_item' data-aos="fade-right">
@@ -53,30 +46,11 @@ export default function Portfolio() {
     <div className="portfolio_item-image">
     <img src={port4}/>
     </div>
-<<<<<<< HEAD
     <h3>Ticket</h3>
 
     <div className="portfolio_item-cta">
     <a href="Esraamohamed2023/Login" className='btn' target="_blank">Gighub</a>
     <a href="https://esraamohamed2023.github.io/ticket/" className='btn btn-primary' target="_blank">Live Demo</a>
-=======
-    <h3>dessert_project</h3>
-
-    <div className="portfolio_item-cta">
-    <a href="Esraamohamed2023/Login" className='btn' target="_blank">Gighub</a>
-    <a href="https://esraamohamed2023.github.io/dessert_project/" className='btn btn-primary' target="_blank">Live Demo</a>
-    </div>
-    </article>
-        <article className='portfolio_item' data-aos="fade-down">
-    <div className="portfolio_item-image">
-    <img src={port4}/>
-    </div>
-    <h3>ecommerce site</h3>
-
-    <div className="portfolio_item-cta">
-    <a href="Esraamohamed2023/Login" className='btn' target="_blank">Gighub</a>
-    <a href="https://esraamohamed2023.github.io/Task-10024/" className='btn btn-primary' target="_blank">Live Demo</a>
->>>>>>> 15105bae0add1c9c82a2f5129dc5ce2f2764bbfb
     </div>
 
     </article>
